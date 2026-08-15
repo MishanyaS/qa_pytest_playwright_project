425e5849912639a9fbca41d2458ebc3d81a0bad8
MishanyaS <shilinm1203@gmail.com>
2026-08-15 11:13:17 +0300

    Add BasePage class

---
+++ pages/base_page.py
@@ -0,0 +1,26 @@
+from playwright.sync_api import Locator, Page
+
+class BasePage:
+    def __init__(self, page: Page):
+        self.page = page
+
+    def goto(self, url: str) -> None:
+        self.page.goto(url)
+
+    def get_title(self) -> str:
+        return self.page.title()
+
+    def get_url(self) -> str:
+        return self.page.url
+
+    def click(self, locator: Locator) -> None:
+        locator.click()
+
+    def fill(self, locator: Locator, value: str) -> None:
+        locator.fill(value)
+
+    def get_text(self, locator: Locator) -> str:
+        return locator.inner_text()
+
+    def is_visible(self, locator: Locator) -> bool:
+        return locator.is_visible()
